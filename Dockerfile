b6342d734f7d5d7c4da210c720ee097fce901905
mcucii <masa.cucic@gmail.com>
2026-05-25 21:13:10 +0200

    fix: dockerfile and strat ports

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,5 @@ COPY . .
 RUN chmod +x /app/start.sh
 
 ENV AWS_REGION=eu-central-1
-EXPOSE 8501 8000
+EXPOSE 8501 8080
 CMD ["/app/start.sh"]    
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-uvicorn api.main:app --host 0.0.0.0 --port 8000 &
+uvicorn api.main:app --host 0.0.0.0 --port 8080 &
 
 python -m streamlit run clients/streamlit_app.py --server.port 8501 --server.address 0.0.0.0
